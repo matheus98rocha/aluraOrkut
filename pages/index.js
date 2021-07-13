d5ffb5abe0488f56f78b24b0b6783ce5673882e6
Matheus Gonçalves <matheus98rocha@gmail.com>
2021-07-12 22:39:06 -0300

    Primeiro dia

---
+++ pages/index.js
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import Box from '../src/components/Box';
+import MainGrid from '../src/components/MainGrid';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations/index';
+
+export default function Home() {
+
+  const githubUser = 'matheus98rocha';
+  const favoritePerson = ['carduardo13', 'torvalds', 'marcusalves13', 'maykbrito'];
+
+  function ProfileSidebar() {
+    return (
+      <Box>
+        <img src={`https://github.com/${githubUser}.png`} style={{ borderRadius: '8px' }} />
+      </Box>
+    )
+  }
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+          <ProfileSidebar />
+        </div>
+
+        <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+          <Box>
+            <h1 className="title">Bem vindo</h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+
+        <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+              Amigos ({favoritePerson.length})
+            </h2>
+            <ul>
+              {
+                favoritePerson.map((person) => {
+                  return (
+                    <li>
+                      <a href={`https://github.com/${person}/`} key={person} target="_Blank">
+                        <img src={`https://github.com/${person}.png`} />
+                        <span>{person}</span>
+                      </a>
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </ProfileRelationsBoxWrapper>
+          <Box>
+            Comunidades
+          </Box>
+        </div>
+      </MainGrid>
+    </>
+  )
+}
